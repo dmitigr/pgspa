23b06ef0aae37359f799fecb5305bb7e0beae629
Dmitry Igrishin <dmitigr@gmail.com>
2019-05-18 14:12:01 +0300

    Regression fix of the CMake stuff
    Stop CMake if no required dependencies found.

--- cmake/FindPgfe.cmake
+++ cmake/FindPgfe.cmake
@@ -17,5 +17,6 @@
 #    misrepresented as being the original software.
 # 3. This notice may not be removed or altered from any source distribution.
 
+set(pkg Pgfe)
 set(lib dmitigr_pgfe)
 include(dmitigr_package_finder)
